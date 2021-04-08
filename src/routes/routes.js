f837bceeaa01504ff91c445d175fecc843c33e3f
Rafael Martins <rafael.martins@poli.ufrj.br>
2021-04-08 17:08:09 -0300

    Second commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,5 @@
 
 const router = require('express').Router();
-const CellphoneController = require('../controllers/CellphoneController');
-const validators = require('../config/validators');
 
-router.post('/cellphones',validators.validateCellphone('create'),CellphoneController.create);
-router.get('/cellphones',CellphoneController.index);
-router.get('/cellphones/:id',CellphoneController.show);
-router.put('/cellphones/:id', validators.validateCellphone('update'),CellphoneController.update);
-router.delete('/cellphones/:id', CellphoneController.delete);
 
-router.put('/acquireCellPhone/:userId/:cellphoneId', CellphoneController.acquireCellPhone);
-router.put('/throwAwayCellPhone/:userId/:cellphoneId', CellphoneController.throwAwayCellPhone);
 module.exports = router;
